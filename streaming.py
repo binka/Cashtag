b6d4361ae5c4aa33e095788b6cda137c3955fa0a
David Bittle <user@cu-cs-vm.(none)>
2015-03-03 18:39:26 -0700

    A way to query the twitter stream

---
+++ streaming.py
@@ -0,0 +1,20 @@
+#pip install tweepy
+#cashtag
+#change count to change the number of tweets returned
+import tweepy
+consumer_key = "43b4urzsW8nMY3oGzB5tIIM8B"
+consumer_secret = "fbGLMhkFyipYbTAz0s0S6yrN6cDGGWnEMmNaciceYjr4sgEdP2"
+
+access_token = "2990432317-eYMpYm2Ck2G1YBPvWEq7Mf9wdgzBlOydabaxmzN"
+access_token_secret = "lQYcmiMlFdic9KSdmd6PClGQ3Swq8y9BgvVPOmqwhHjV2"
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+
+myCount = 100
+results = api.search(q="Apple stock",count = myCount)
+
+for result in results:
+    print (result.text)
