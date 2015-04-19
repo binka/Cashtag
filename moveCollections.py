3570ace6ec6855df41929dd918f31fe8e2f15a80
Lincoln Samelson <sirbinkai@gmail.com>
2015-04-19 16:47:39 -0600

    Testing printing collection names for script

--- moveCollections.py
+++ moveCollections.py
@@ -1,3 +1,11 @@
 # Team Cashtag
-# db.runCommand({renameCollection:"cashtag.stockname",to:"cleancashtag.stockname"})
-# that is the command to move "stockname" from cashtag collection to cleancashtag
+import pymongo
+client = pymongo.MongoClient("54.149.244.192", 27017)
+db = client.cashtag
+collection = db_name.get_db().collection_name
+print collection
+#for x in range(0, lastcollection
+	#if (name == "StockTwits2010" || "StockTwits2011" || "StockTwits2012" || "StockTwits2013" || "StockTwits2014" )
+	# that is the command to move "stockname" from cashtag collection to cleancashtag
+	#db.runCommand({renameCollection:"cashtag.stockname",to:"cleancashtag.stockname"})
+
